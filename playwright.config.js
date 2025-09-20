15d929ee9e5ec866edb4421a6df23a6f09b8e31c
Nico Krause <post@nicokrause.com>
2025-09-20 16:32:37 +0200

    fix starting another preview webserver although playwright started one already

--- playwright.config.js
+++ playwright.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   /* Shared settings for all the tests. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -55,9 +55,11 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'cd examples/webauthn-todo-demo && npm run dev',
+    command: process.env.CI 
+      ? 'cd examples/webauthn-todo-demo && npm run preview -- --port 5173 --host'
+      : 'cd examples/webauthn-todo-demo && npm run dev',
     url: 'http://localhost:5173',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: true, // Always reuse existing server
     timeout: 120 * 1000,
   },
 });
